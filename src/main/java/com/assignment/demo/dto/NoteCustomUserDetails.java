0d0e001600e01c3dd8e74a889cd85a8e242e3aad
Amit Gupta <vinodbagga9@gmail.com>
2022-10-30 22:57:30 +0530

    added NoteCustomUserDetails java file

---
+++ src/main/java/com/assignment/demo/dto/NoteCustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.assignment.demo.dto;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/*
+ * 	this class is used for : provides  user information using the UserDetails interface. 
+ * simply store user information which is later encapsulated into Authentication objects.
+ * 
+ * 
+ * */
+public class NoteCustomUserDetails implements UserDetails {
+
+	
+	private static final long serialVersionUID = 2336318806804844013L;
+	
+	private User user;
+	
+	
+
+	public NoteCustomUserDetails(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
+
